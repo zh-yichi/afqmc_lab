44d4237491ae72e848da1d3f1d900eef91bd2157
yichi <zhangych98@gmail.com>
2026-06-14 20:13:25 -0700

    added some my lno

--- experiments/test_lno/run_one_frag.py
+++ experiments/test_lno/run_one_frag.py
@@ -1,18 +1,18 @@
 from afqmc.lno_afqmc import lno_afqmc
 
-options = {'eql_time': 10,
-           'n_blocks': 50,
-           'n_walkers': 300,
-           'max_memory': 1000,
-           'mix_precision': False,
-           'n_batch': 1,
-           'seed': 17,
-           'walker_type': 'rhf',
-           'trial': 'pt2ccsd',
-           'dt':0.005,
-           }
+# options = {'eql_time': 10,
+#            'n_blocks': 50,
+#            'n_walkers': 300,
+#            'max_memory': 1000,
+#            'mix_precision': False,
+#            'n_batch': 1,
+#            'seed': 17,
+#            'walker_type': 'uhf',
+#            'trial': 'upt2ccsd',
+#            'dt':0.005,
+#            }
 
-lno_afqmc.run_lnoafqmc(options)
+# lno_afqmc.run_lnoafqmc(options)
 
 options = {'eql_time': 10,
            'n_blocks': 50,
@@ -21,24 +21,24 @@ options = {'eql_time': 10,
            'mix_precision': True,
            'n_batch': 1,
            'seed': 17,
-           'walker_type': 'rhf',
-           'trial': 'pt2ccsd',
+           'walker_type': 'uhf',
+           'trial': 'upt2ccsd',
            'dt':0.005,
            }
 
 lno_afqmc.run_lnoafqmc(options)
 
-options = {'eql_time': 10,
-           'n_blocks': 50,
-           'n_walkers': 300,
-           'max_memory': 1000,
-           'mix_precision': False,
-           'n_batch': 1,
-           'seed': 17,
-           'walker_type': 'rhf',
-           'trial': 'pt2ccsd_ad',
-           'dt':0.005,
-           }
+# options = {'eql_time': 10,
+#            'n_blocks': 50,
+#            'n_walkers': 300,
+#            'max_memory': 1000,
+#            'mix_precision': False,
+#            'n_batch': 1,
+#            'seed': 17,
+#            'walker_type': 'uhf',
+#            'trial': 'upt2ccsd_ad',
+#            'dt':0.005,
+#            }
 
-lno_afqmc.run_lnoafqmc(options)
+# lno_afqmc.run_lnoafqmc(options)
 
--- experiments/test_lno/run_ulnoafqmc.py
+++ experiments/test_lno/run_ulnoafqmc.py
@@ -7,56 +7,59 @@ lno_thresh = lno_list[lno_num-1]
 
 ####  test H2 monomers ####
 a = 2 # bond length in a cluster
-d = 4 # distance between each cluster
+d = 3 # distance between each cluster
 unit = 'b' # unit of length
 na = 2 # size of a cluster (monomer)
 nc = 5 # set as integer multiple of monomers
-spin = 0 # spin per monomer
+spin = 0*nc # spin per monomer
 frozen = 0 # frozen orbital per monomer
-elmt = 'H'
+elmt = 'N'
 unit = 'B'
 basis = 'sto6g'
 atoms = ""
 for n in range(nc*na):
     shift = ((n - n % na) // na) * (d-a)
     atoms += f"{elmt} {n*a+shift:.5f} 0.00000 0.00000 \n"
+
 ###########################
+# atoms = '''
+# O   -1.485163346097   -0.114724564047    0.000000000000
+# H   -1.868415346097    0.762298435953    0.000000000000
+# H   -0.533833346097    0.040507435953    0.000000000000
+# O    1.416468653903    0.111264435953    0.000000000000
+# H    1.746241653903   -0.373945564047   -0.758561000000
+# H    1.746241653903   -0.373945564047    0.758561000000
+# '''
+# unit = "A"
+# spin = 0
 
 mol = gto.M(atom=atoms,
-            basis="sto6g",
+            basis="ccpvdz",
             verbose=4,
             unit=unit,
             symmetry=0,
             charge=0,
-            spin=spin*nc,
+            spin=spin,
             max_memory=40000,
             )
 
 mf = scf.UHF(mol).density_fit()
 mf.kernel()
 
-# from pyscf.lno.tools import autofrag_iao
-# from pyscf import lo
-# import numpy as np
-# from pyscf.data import elements
-
-# frozen = elements.chemcore(mol)
-
-# # IAO localization
-# orbocc_a = mf.mo_coeff[0][:,frozen:np.count_nonzero(mf.mo_occ[0])]
-# orbocc_b = mf.mo_coeff[1][:,frozen:np.count_nonzero(mf.mo_occ[1])]
-# lo_coeff_a = lo.iao.iao(mol, orbocc_a)
-# lo_coeff_a = lo.orth.vec_lowdin(lo_coeff_a, mf.get_ovlp())
-# lo_coeff_b = lo.iao.iao(mol, orbocc_b)
-# lo_coeff_b = lo.orth.vec_lowdin(lo_coeff_b, mf.get_ovlp())
-# lo_coeff = [lo_coeff_a, lo_coeff_b]
-# moliao = lo.iao.reference_mol(mol)
-# frag_lolist = autofrag_iao(moliao)
-# frag_lolist = [[i,i] for i in frag_lolist]
+stable = False
+while not stable:
+    print(f'mf stability test')
+    if not stable:
+        mo_i, _, stable,_ = mf.stability(return_status=True)
+        dm = mf.make_rdm1(mo_i,mf.mo_occ)
+        mf.kernel(dm0=dm)
+    elif stable:
+        print(f'mf energy: {mf.e_tot}, stability {stable}')
+        break
 
 from afqmc.lno_afqmc import tools, lno_afqmc
-
 lo_coeff, frag_lolist, atm_center = tools.iao_localization(mf)
+from pyscf.data import elements
 
 options = {
            'eql_time': 10,
@@ -72,12 +75,12 @@ lno_afqmc.run_afqmc(
               mf,
               lo_coeff = lo_coeff,
               frag_lolist = frag_lolist,
-              nfrozen = frozen,
+              nfrozen = elements.chemcore(mol),
               thresh = lno_thresh,
               qmc_options = options,
               chol_cut = 1e-5,
               target_sto_error = 5e-4,
-              run_frag_list = None,
+              run_frag_list = [0],
               atom_group = atm_center,
               plot_las = True,
               )
